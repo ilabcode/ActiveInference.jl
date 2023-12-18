1465988920c2674b0d980dd7fcc6d81a8ca86aa0
Jonathan7773 <jonathan.e.laursen@gmail.com>
2023-12-18 14:50:26 +0100

    test 2

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,14 @@
-using ActiveInference
-using Test
+#using ActiveInference
+#using Test
 
 
-# # ---test the functions here---
-# categorical_dist = rand(3)
-# categorical_dist = maths.norm_dist(categorical_dist)
-# # --- end of testing
+# ---test the functions here---
+#categorical_dist = rand(3)
+#categorical_dist = norm_dist(categorical_dist)
+# --- end of testing
 
 
 
-# @testset "ActiveInference.jl" begin
+@testset "ActiveInference.jl" begin
       
-# end
+end
