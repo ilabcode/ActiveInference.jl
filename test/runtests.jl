e8375a1585477c103e41aa1eff26f194daf399a0
Jonathan7773 <jonathan.e.laursen@gmail.com>
2023-12-18 15:05:41 +0100

    test 7

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
-#using ActiveInference
-#using Test
+using ActiveInference
+using Test
 
 
-# ---test the functions here---
-#categorical_dist = rand(3)
-#categorical_dist = norm_dist(categorical_dist)
-# --- end of testing
+---test the functions here---
+categorical_dist = rand(3)
+categorical_dist = norm_dist(categorical_dist)
+--- end of testing
 
 
 
