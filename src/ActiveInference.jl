17dab4d295902ab9ed9748fb9fb52344426b9269
Jonathan7773 <jonathan.e.laursen@gmail.com>
2023-12-18 13:44:01 +0100

    test

--- src/ActiveInference.jl
+++ src/ActiveInference.jl
@@ -3,24 +3,24 @@ module ActiveInference
 include("functions.jl")
 include("maths.jl")
 
-#from functions.jl
-export plot_beliefs
-export plot_gridworld
-export plot_likelihood
-export create_B_matrix
-export onehot
-export plot_point_on_grid
-export infer_states
-export get_expected_states
-export get_expected_observations
+# From functions.jl
+# export plot_beliefs
+# export plot_gridworld
+# export plot_likelihood
+# export create_B_matrix
+# export onehot
+# export plot_point_on_grid
+# export infer_states
+# export get_expected_states
+# export get_expected_observations
 
 
-#from maths.jl
-export norm_dist
-export sample_category
-export softmax
-export spm_log_single
-export entropy
+# # From maths.jl
+# export norm_dist
+# export sample_category
+# export softmax
+# export spm_log_single
+# export entropy
 
 
 
