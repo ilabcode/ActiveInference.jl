542e142afb0a41ca1d79996fcc2062b04d67b2b9
Jonathan7773 <jonathan.e.laursen@gmail.com>
2023-12-19 10:55:12 +0100

    test 2

--- src/ActiveInference.jl
+++ src/ActiveInference.jl
@@ -1,32 +1,33 @@
 module ActiveInference
 
-    include("functions.jl")
+include("functions.jl")
 
-    # From functions.jl
-    export plot_beliefs
-    export plot_gridworld
-    export plot_likelihood
-    export create_B_matrix
-    export onehot
-    export plot_point_on_grid
-    export infer_states
-    export get_expected_states
-    export get_expected_observations
-    export calculate_G
-    export run_active_inference_loop
-    export construct_policies
-    export calculate_G_policies
-    export compute_prob_actions
-    export active_inference_with_planning
+# From functions.jl
+export plot_beliefs, plot_gridworld, plot_likelihood, create_B_matrix, onehot, plot_point_on_grid, infer_states, get_expected_states, get_expected_observations, calculate_G, run_active_inference_loop, construct_policies, calculate_G_policies, compute_prob_actions,active_inference_with_planning
+
+    # export plot_likelihood
+    # export create_B_matrix
+    # export onehot
+    # export plot_point_on_grid
+    # export infer_states
+    # export get_expected_states
+    # export get_expected_observations
+    # export calculate_G
+    # export run_active_inference_loop
+    # export construct_policies
+    # export calculate_G_policies
+    # export compute_prob_actions
+    # export active_inference_with_planning
 
 end
 
 
 module maths
     
-    include("maths.jl")
+include("maths.jl")
 
-    # export norm_dist
+export norm_dist, sample_category, softmax, spm_log_single, entropy, kl_divergence
+    
     # export sample_category
     # export softmax
     # export spm_log_single
@@ -38,11 +39,11 @@ end
 
 module environment
 
-    include("environment.jl")
+include("environment.jl")
 
-    export GridWorldEnv
-    export step!
-    export reset!
+export GridWorldEnv, step!, reset!
+    # export step!
+    # export reset!
 
 end
 
