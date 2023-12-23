298b9dfbfcaa0131b8192381734ee91aef600b35
Jonathan7773 <jonathan.e.laursen@gmail.com>
2023-12-23 15:31:12 +0100

    test 9

--- src/functions.jl
+++ src/functions.jl
@@ -161,9 +161,9 @@ function infer_states(observation_index, A, prior)
 
     log_prior = spm_log_single(prior)
 
-    qs = softmax(log_likelihood .+ log_prior)
+    qs_current = softmax(log_likelihood .+ log_prior)
 
-    return qs
+    return qs_current
 end
 
 
