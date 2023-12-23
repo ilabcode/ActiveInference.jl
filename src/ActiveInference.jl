9f1966b5b1da2b04fd2bad6076ddcc47c1689fc2
Jonathan7773 <jonathan.e.laursen@gmail.com>
2023-12-23 16:10:33 +0100

    test 14

--- src/ActiveInference.jl
+++ src/ActiveInference.jl
@@ -5,7 +5,7 @@ include("maths.jl")
 include("environment.jl")
 
 # From functions.jl
-export array_of_any, array_of_any_zeros, plot_beliefs, plot_gridworld, plot_likelihood, create_B_matrix, onehot, plot_point_on_grid, infer_states, get_expected_states, get_expected_observations, calculate_G, run_active_inference_loop, construct_policies, calculate_G_policies, compute_prob_actions,active_inference_with_planning
+export array_of_any, array_of_any_zeros, plot_beliefs, plot_gridworld, plot_likelihood, create_B_matrix, onehot, plot_point_on_grid, infer_states, get_expected_states, get_expected_observations, calculate_G, run_active_inference_loop, construct_policies, calculate_G_policies, compute_prob_actions,active_inference_with_planning, GridWorldEnv, step!, reset!
 
     # From maths.jl
     module Maths
