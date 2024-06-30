989e101c4489e124d72f0eeb7d8a3cc146bacc04
Samuel William Nehrer <nehrer.samuel@gmail.com>
2024-06-30 18:52:56 +0200

    Environment - Type Adjusted

--- src/Environments/TMazeEnv.jl
+++ src/Environments/TMazeEnv.jl
@@ -166,7 +166,7 @@ function construct_likelihood_dist(env::TMazeEnv)
     return A
 end
 
-function sample_dist(probabilities::Vector{Float64})
+function sample_dist(probabilities)
 
     # Ensure probabilities sum to 1
     probabilities /= sum(probabilities)
