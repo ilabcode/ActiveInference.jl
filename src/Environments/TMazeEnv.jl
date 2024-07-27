e20d8355ebe2087574de214eeeba5e2ade279a93
Jonathan7773 <jonathan.e.laursen@gmail.com>
2024-07-27 13:16:35 +0200

    TMaze fixing

--- src/Environments/TMazeEnv.jl
+++ src/Environments/TMazeEnv.jl
@@ -166,7 +166,9 @@ function construct_likelihood_dist(env::TMazeEnv)
     return A
 end
 
-function sample_dist(probabilities::Union{Vector{<:Real}, Vector{Any}, Vector{Float64}})
+function sample_dist(probabilities::Vector{<:Real})
+
+    probabilities = convert(Vector{Float64}, probabilities)
 
     # Ensure probabilities sum to 1
     probabilities /= sum(probabilities)
