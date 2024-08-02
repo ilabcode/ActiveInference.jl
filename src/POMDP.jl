89355db4cb2add15df07ef73dc2c5cf6ad2ae666
samuelnehrer02 <nehrer.samuel@gmail.com>
2024-08-02 11:13:57 +0200

    Action Model Update

--- src/POMDP.jl
+++ src/POMDP.jl
@@ -6,43 +6,50 @@ This module contains models of Partially Observable Markov Decision Processes un
 
 ### Action Model:  Returns probability distributions for actions per factor
 
-# NOTE: Add action_distribution -> aif.states["action_probability"]
-
 function action_pomdp!(agent::Agent, obs::Vector{Int64})
 
-
-    # Small change
     aif = agent.substruct
 
     ### Get parameters 
     alpha = agent.parameters["alpha"]
-    n_factors = length(agent.settings["num_controls"])
+    n_factors = length(aif.settings["num_controls"])
 
-    #gamma = agent.parameters["gamma"]
-    #aif.gamma = gamma
-
-    # Initialize an empty arrays for action distribution per factor
+    # Initialize empty arrays for action distribution per factor
     action_p = array_of_any(n_factors)
     action_distribution = Vector(undef, n_factors)
+
+    #If there was a previous action
+    if !ismissing(agent.states["action"])
+
+        #Extract it
+        previous_action = agent.states["action"]
+
+        #If it is not a vector, make it one
+        if !(previous_action isa Vector)
+            previous_action = [previous_action]
+        end
+
+        #Store the action in the AIF substruct
+        agent.substruct.action = previous_action
+    end
 
     ### Infer states & policies
 
     # Run state inference 
     infer_states!(aif, obs)
-    #update_A!(aif, obs)
 
     # Run policy inference 
     infer_policies!(aif)
 
     ### Retrieve log marginal probabilities of actions
-    log_action_marginals = get_log_action_marginals(aif)
+    log_action_marginals = ActiveInference.get_log_action_marginals(aif)
 
     ### Pass action marginals through softmax function to get action probabilities
     for factor in 1:n_factors
         action_p[factor] = softmax(log_action_marginals[factor] * alpha)
         action_distribution[factor] = Distributions.Categorical(action_p[factor])
     end
-
+    
     return action_distribution
 end
 
