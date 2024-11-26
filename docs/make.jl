394089917bdd84285c09d9be5380281d6b6be98e
Jonathan7773 <jonathan.e.laursen@gmail.com>
2024-11-26 15:42:24 +0100

    Temporary Fix

--- docs/make.jl
+++ docs/make.jl
@@ -4,25 +4,25 @@ using Literate
 
 # cd(raw"C:\Users\Jonathan Laursen\Desktop\University\Bachelor\ActiveInference.jl\docs\src")
 
-if haskey(ENV, "GITHUB_WORKSPACE")
-    project_dir = ENV["GITHUB_WORKSPACE"]
-else
-    project_dir = pwd()
-end
+# if haskey(ENV, "GITHUB_WORKSPACE")
+#     project_dir = ENV["GITHUB_WORKSPACE"]
+# else
+#     project_dir = pwd()
+# end
 
-cd(joinpath(project_dir, "docs", "src"))
+# cd(joinpath(project_dir, "docs", "src"))
 
 DocMeta.setdocmeta!(ActiveInference, :DocTestSetup, :(using ActiveInference); recursive=true)
 
 # Automating the creating of the markdown files
-input_folder = raw"..\julia_files"
+# input_folder = raw"..\julia_files"
 
-julia_files = filter(file -> endswith(file, ".jl"), readdir(input_folder))
+# julia_files = filter(file -> endswith(file, ".jl"), readdir(input_folder))
 
-for file in julia_files
-    input_path = joinpath(input_folder, file)
-    Literate.markdown(input_path, outputdir="", execute=true, documenter=true, codefence =  "```julia" => "```")
-end
+# for file in julia_files
+#     input_path = joinpath(input_folder, file)
+#     Literate.markdown(input_path, outputdir="", execute=true, documenter=true, codefence =  "```julia" => "```")
+# end
 
 # Creating the documentation
 makedocs(;
